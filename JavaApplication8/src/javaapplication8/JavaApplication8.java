c062ceee61b140638e2a2f4c703eeab5df96d056
magelac94 <magelac94@gmail.com>
2018-10-08 20:58:18 -0200

    hola mundo2

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -19,6 +19,7 @@ public class JavaApplication8 {
     public static void main(String[] args) {
         // TODO code application logic here
         println("hola mundo");
+        println("hola mundo");
     }
     
 }
