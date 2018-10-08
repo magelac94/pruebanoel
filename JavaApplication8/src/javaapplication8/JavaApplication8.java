a867c0794826e6d7f7191a067ae26db879c0bcb1
magelac94 <magelac94@gmail.com>
2018-10-08 20:55:31 -0200

    hola mundo

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -5,6 +5,8 @@
  */
 package javaapplication8;
 
+import static java.sql.DriverManager.println;
+
 /**
  *
  * @author Magela
@@ -16,6 +18,7 @@ public class JavaApplication8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        println("hola mundo");
     }
     
 }
